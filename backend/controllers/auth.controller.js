01f2ad2c88e681c8be7787d0f4c0eb0e759aa4f8
DarshK25 <darshkalathiya25@gmail.com>
2024-10-07 22:04:20 +0530

    Updating Code

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -30,11 +30,27 @@ export const signup = async(req, res) => {
         
         await user.save(); //Saved to user database!!!!
         res.status(201).json({message: "User created successfully"});
+        const token = jwt.sign( {userID: user._id}, process.env.JWT_SECRET ); 
+        //generating token which is used for authentication which means the user is logged in
+        //jwt.sign is a method to sign token and has parameters payload(an object), secret key and options
+        //secret key stored in .env file 
+        // a cookie is a small piece of data that is sent with every request to the server 
+        //and stored in the browser, it helps us by sending a token to the server and the server verifies token 
+        
+        res.cookie("jwt-apsitin", token, {
+            httpOnly: true, // only accessible by the server and prevents XSS attack(cross-side scripting) 
+            //and does not allow users to access this cookie
+            maxAge: 3 * (24 * 60 * 60 * 1000), // This is in milliseconds which is equal to 3*24 hours, This is token's validity age
+            sameSite: "strict", // prevents CSRF / XSRF attacks, these attack
+            secure: process.env.NODE_ENV === "production", // This means that the cookie will only be sent over HTTPS & prevents man-in-the-middle attacks
+        });
 
-        // const token = jwt.sign({userID: })
+        res.status(201).json({message: "User registered succesfully"}); 
+        //todo: send welcome email
     }
     catch(error){
-         
+         console.log("Error in signup", error.message);
+         res.status(500).json({message: "Internal server error"});
     }
 }
 export const login = async(req, res) => {
