5d7633c82ef6c51cef805c160042bc4a6ea4fa73
SaishJoshi <saishjoshi0402@gmail.com>
2024-10-08 20:10:06 +0530

    remaining changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import {connectDB} from "./lib/db.js";
 
@@ -9,6 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json()); //middleware, which means we can access req.body which is JSON data
 
 app.use("/api/v1/auth", authRoutes); //endpoint
